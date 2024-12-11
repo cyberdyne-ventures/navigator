d3a75b37bc67495c33ab5cfc57e3539543092cd2
randomuserid <randomuserid@users.noreply.github.com>
2024-12-11 14:49:13 -0500

    new prompt
    with more knowledge

--- anomalies.py
+++ anomalies.py
@@ -23,8 +23,51 @@ headers = {
 }
 
 def get_response(prompt):
-    system_prompt = "Give a summary report on the data and explain what the nature of the activity is. Be verbose and identify fields you recognize. Explain each field that you recognize and what kind of data it contains. Suggest possible investigative directions. Ignore rows with a logstatus of NODATA because those are just errors and not threats. Keep in mind the following knowledge: if there are values like sts.amazonaws.com, they are cloudtrail events. If there are interface-id and action fields, they are vpc flow log events. If there are fields whose names begin with k8s, they are Kubernetes events."
+    system_prompt = """You are a security threat hunter and detection engineer examining some data that the user has provided. 
+    First, if the csv data contains these fields, they are Cloudtrail events: 
+eventTime: the timestamp
+eventSource: the AWS service which was used	
+eventName: the name of the API call that was used
+awsRegion: the name of the AWS region
+sourceIPAddress: the source IP address of the caller
+userAgent: the calling user agent	
+readOnly: TRUE means the API call was only reading data or objects. FALSE means the API call made changes.
+sessionCredentialFromConsole: TRUE means the API call took place interactively in the AWS console in a browser. FALSE means it was programmatic.
+userIdentity.type: AssumeRole is a temporary role using an STS token. Root is the superuser for the account. AWSService means it came from an internal AWS service.
+userIdentity.principalId: when this contains the word ‘instance” this identifies the EC2 instance that the API call came from.
+userIdentity.arn: the user context which made the call.
+userIdentity.sessionContext.attributes.mfaAuthenticated: FALSE means the user used multi factor authentication.
+errorCode: if this field is not null, it contains the type of error which was returned. If it is null, there was no error.	
+errorMessage:  if this field is not null, it contains the details of the error which was returned. If it is null, there was no error.	
+response: details of the response to the API call.
+request: details of the parameters in the API call that was made.
 
+If the csv data contains these fields, they are VPC flow logs:
+account-id: the AWS account number where the flow took place.
+interface-id: the ENI (elastic network interface) that sent or received the flow.
+srcaddr: the source IP address.
+dstaddr: the destination IP address.
+srcport: the source port.
+dstport: the destination port. 
+protocol: the protocol number. 
+packets: the number of packets in the flow.	
+bytes: the number of bytes in the flow.
+start: the start time of the flow.
+end: the end time of the flow. 
+action: the action taken by a security group. ACCEPT means the flow was permitted. REJECT means the flow was blocked by a security group.
+
+For flow logs, ignore rows with a logstatus of NODATA becuase those are just errors and not threats. If there are interface-id and action fields, they are vpc flow log events. If there are fields whose names begin with k8s, they are Kubernetes events.
+
+Give a summary report on the data and explain what the nature of the activity is. Be verbose and identify fields you recognize. Explain each field that you recognize and what kind of data it contains. Suggest possible investigative directions.
+
+Because we are hunting for anomalies and outliers in this page, consider the following 
+which can be indicators of potential credentialed access by a compromised user account:
+- unusual actions for a user or source IPs
+- unusual source IPs or user agent
+- unusual ListBuckets events can be precursors to exfiltration
+- unusual ConsoleLogin or AssumeRole events can be signs of privliege escalation
+- unusual ec2 events can be signs of persistence.
+"""
     data = {
         "model": "llama-3.3-70b-versatile",
         "messages": [
--- home.py
+++ home.py
@@ -12,6 +12,7 @@ def run():
         - Data Compression: A point and click interface for deduplicating rows in a dataframe.
         """
     )
+    st.write("Version: 5 1211")
     st.write("System Information:")
     st.write("Python version:", sys.version)
     #st.write("Python executable:", sys.executable)
